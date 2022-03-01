43abda6c0813b269b1ca6cc34783108446de95db
Robert Dzudzar <33647339+rdzudzar@users.noreply.github.com>
2022-03-01 20:18:36 +1100

    adding ttl to cache

--- functions.py
+++ functions.py
@@ -180,9 +180,11 @@ def get_cmaps_from_origin(origin):
 #          allow_output_mutation=True,
 #          suppress_st_warning=True)
 
+# adding time to live - ttl: cache objects will be removed after 5hours
+# testing whether this will save app from reaching resource limits
 @st.cache(hash_funcs={matplotlib.figure.Figure: lambda _: None},
                     allow_output_mutation=True,
-                    suppress_st_warning=True)
+                    suppress_st_warning=True, ttl=5*3600)
 def colormap_figure(colormap, origin, cmap_color_span, num_of_swatches):
     """
     Get the origin of the colormap and greate a plot of colormap, using
