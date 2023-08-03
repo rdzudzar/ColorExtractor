4b88b03ab8bf32c024f76d1257a82f422598c47d
Robert Dzudzar <33647339+rdzudzar@users.noreply.github.com>
2023-08-03 12:43:53 +1000

    pyart compatibility issues

--- functions.py
+++ functions.py
@@ -21,7 +21,7 @@ import cmcrameri as cmc
 import cmocean as cmo
 import colorcet as cc
 
-from pyart.testing import get_test_data
+#from pyart.testing import get_test_data
 
 # For Procreate swatches
 import json
@@ -126,14 +126,14 @@ def get_cmaps_from_origin(origin):
                           'browns4', 'browns5', 'browns6', 'browns7', 
                           'browns8', 'browns9']
 
-    elif origin == "pyart":
-        
-        colormap_names = ['LangRainbow12', 'HomeyerRainbow', "BuDOr18",  
-                            "BuOr10", "BuOr12", "BuOr8", "BuDOr12", "BuDRd12","BuDRd18","BuOrR14",
-                            "RdYlBu11b", 'balance',"Bu10", "Bu7",
-                            "Gray5", "Gray9", "SymGray12", "BuGy8", "BlueBrown11", "BrBu10", "BrBu12",
-                             "BuGr14", "GrMg16","Carbone11", "Carbone17",
-                             "RRate11","StepSeq25","Theodore16"]
+    #elif origin == "pyart":
+    #    
+    #    colormap_names = ['LangRainbow12', 'HomeyerRainbow', "BuDOr18",  
+    #                        "BuOr10", "BuOr12", "BuOr8", "BuDOr12", "BuDRd12","BuDRd18","BuOrR14",
+    #                        "RdYlBu11b", 'balance',"Bu10", "Bu7",
+    #                        "Gray5", "Gray9", "SymGray12", "BuGy8", "BlueBrown11", "BrBu10", "BrBu12",
+    #                         "BuGr14", "GrMg16","Carbone11", "Carbone17",
+    #                         "RRate11","StepSeq25","Theodore16"]
 
         
     elif origin == "colorcet":        
@@ -243,9 +243,9 @@ def colormap_figure(colormap, origin, cmap_color_span, num_of_swatches):
         cmap_range = cmr.get_sub_cmap('cmc.'+f"{colormap}", 
                              cmap_color_span[0], cmap_color_span[1])
 
-    elif origin == 'pyart':
-        cmap_range = cmr.get_sub_cmap('pyart_'+f"{colormap}", 
-                             cmap_color_span[0], cmap_color_span[1])
+    #elif origin == 'pyart':
+    #    cmap_range = cmr.get_sub_cmap('pyart_'+f"{colormap}", 
+    #                         cmap_color_span[0], cmap_color_span[1])
         
     else:
         cmap_range = cmr.get_sub_cmap(f"{colormap}", 
@@ -399,9 +399,9 @@ def swatcheslike(cmp_name, origin, cmap_color_span, num_of_swatches):
     elif origin == 'crameri':
         cmap_range = cmr.get_sub_cmap('cmc.'+f"{cmp_name}", 
                              cmap_color_span[0], cmap_color_span[1])        
-    elif origin == 'pyart':
-        cmap_range = cmr.get_sub_cmap('pyart_'+f"{cmp_name}", 
-                             cmap_color_span[0], cmap_color_span[1])  
+    #elif origin == 'pyart':
+    #    cmap_range = cmr.get_sub_cmap('pyart_'+f"{cmp_name}", 
+    #                         cmap_color_span[0], cmap_color_span[1])  
 
     else:
         cmap_range = cmr.get_sub_cmap(f"{cmp_name}", 
@@ -462,11 +462,11 @@ def pick_hex(cmp_name, origin, cmap_color_span, num_of_swatches):
                                   cmap_range=(cmap_color_span[0], 
                                               cmap_color_span[1]),
                                   return_fmt='hex')
-    elif origin == 'pyart':
-        colors_hex = cmr.take_cmap_colors('pyart_'+f"{cmp_name}", num_of_swatches, 
-                                  cmap_range=(cmap_color_span[0], 
-                                              cmap_color_span[1]),
-                                  return_fmt='hex')
+    #elif origin == 'pyart':
+    #    colors_hex = cmr.take_cmap_colors('pyart_'+f"{cmp_name}", num_of_swatches, 
+    #                              cmap_range=(cmap_color_span[0], 
+    #                                          cmap_color_span[1]),
+    #                              return_fmt='hex')
 
     else:
         colors_hex = cmr.take_cmap_colors(f"{cmp_name}", num_of_swatches, 
